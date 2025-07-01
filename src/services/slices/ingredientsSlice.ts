78ccf75595de742756065450c827673d4abf178e
Daniel K <89999804331@yandex.ru>
2025-07-02 02:21:54 +0300

    fix: ingredientsSlice

--- src/services/slices/ingredientsSlice.ts
+++ src/services/slices/ingredientsSlice.ts
@@ -1,3 +1,4 @@
+/* eslint-disable prettier/prettier */
 import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
 import { getIngredientsApi } from '../../utils/burger-api';
 import { TIngredient } from '../../utils/types';
