a75930377bc1eddd54fae284d15da665bf6044d1
Daniel K <89999804331@yandex.ru>
2025-07-19 11:28:03 +0300

    fix: cypress tests

--- cypress/e2e/modals/ingredient-modal.cy.ts
+++ cypress/e2e/modals/ingredient-modal.cy.ts
@@ -1,30 +1,36 @@
-describe('Модальное окно ингридента', () => {
+describe('Тестирование модального окна ингредиента', () => {
   beforeEach(() => {
     cy.intercept('GET', 'api/ingredients', { fixture: 'ingredients.json' }).as(
       'getIngredients'
     );
     cy.visit('/');
+    cy.wait('@getIngredients');
   });
+
   afterEach(() => {
     window.localStorage.removeItem('refreshToken');
     cy.clearCookies();
   });
-  test('Должно происходить и закрытие модального окна', () => {
+
+  it('Должно открываться и закрываться по клику на крестик', () => {
     cy.get('[data-testid="ingredient"]').first().click();
-    cy.get('[data-testid="modal"]').should('exist');
+    cy.get('[data-testid="modal"]').should('be.visible');
     cy.get('[data-testid="modal-close"]').click();
     cy.get('[data-testid="modal"]').should('not.exist');
+  });
+
+  it('Должно закрываться по клику на оверлей', () => {
     cy.get('[data-testid="ingredient"]').first().click();
     cy.get('[data-testid="modal-overlay"]').click({ force: true });
     cy.get('[data-testid="modal"]').should('not.exist');
   });
 
-  test('Должны отобразиться корректные детали ингридента', () => {
+  it('Должно отображать корректные данные ингредиента', () => {
     cy.get('[data-testid="ingredient"]').first().click();
     cy.fixture('ingredients.json').then((ingredients) => {
       const ingredient = ingredients.data[0];
       cy.get('[data-testid="ingredient-name"]').should(
-        'contain',
+        'have.text',
         ingredient.name
       );
       cy.get('[data-testid="ingredient-price"]').should(
